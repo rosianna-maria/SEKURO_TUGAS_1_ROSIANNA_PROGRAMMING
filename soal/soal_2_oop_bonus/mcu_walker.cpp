cd472bc310ff69a7ce2032eb3a84b10a36a57ac8
rosianna-maria <rosi.slhi9@gmail.com>
2026-03-25 20:26:28 +0700

    mcu_walker.cpp

--- soal/soal_2_oop_bonus/mcu_walker.cpp
+++ soal/soal_2_oop_bonus/mcu_walker.cpp
@@ -1,3 +1,40 @@
-
 #include "mcu_walker.hpp"
-//[TODO]: Implementasikan kelas mcu_walker dibawah ini
+using namespace std;
+
+mcu_walker::mcu_walker(float cpu_speed, int memory, string os, string nama, int volt, int motor_speed, int rotation)
+    : MCU(cpu_speed, memory, os, nama, volt), motor_speed(motor_speed), rotation(rotation) {
+    cout << "[KONSTRUKTOR WALKER] : Sistem gerak " << name << " telah diinisialisasi." << endl;
+}
+
+mcu_walker::~mcu_walker() {
+    cout << "[DESTRUKTOR WALKER] : Komponen walker " << name << " telah dimatikan." << endl;
+}
+
+void mcu_walker::showSpek() {
+    cout << "[SPEK MCU_WALKER] : " << name << endl;
+    cout << "- CPU      : " << cpu_speed << " GHz" << endl;
+    cout << "- RAM      : " << memory << " MB" << endl;
+    cout << "- OS       : " << os << endl;
+    cout << "- Volt     : " << volt << " V" << endl;
+    cout << "- Speed    : " << motor_speed << " RPM" << endl;
+    cout << "- Rotation : " << rotation << " Deg" << endl;
+}
+
+void mcu_walker::rotate(int rot) {
+    rotation = rot;
+    cout << "[WALKER] : " << name << " berputar ke arah " << rotation << " derajat." << endl;
+}
+
+void mcu_walker::increase_speed(int speed) {
+    motor_speed += speed;
+    cout << "[WALKER] : Kecepatan motor " << name << " naik menjadi " << motor_speed << " RPM." << endl;
+}
+
+void mcu_walker::nambah_volt(int penambahan_voltase) {
+    MCU::nambah_volt(penambahan_voltase);
+}
+
+void mcu_walker::ganti_os(string new_os) {
+    MCU::ganti_os(new_os);
+    cout << "[WALKER UPDATE] : " << name << " migrasi OS ke " << new_os << endl;
+}
