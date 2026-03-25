adc6fdfadf038f025054db8eee431420c1035106
rosianna-maria <rosi.slhi9@gmail.com>
2026-03-25 20:23:13 +0700

    mcu.cpp

--- soal/soal_2_oop_bonus/mcu.cpp
+++ soal/soal_2_oop_bonus/mcu.cpp
@@ -1,9 +1,26 @@
 #include "mcu.hpp"
 using namespace std;
-// [TODO] : Implementasikan class robot di file cpp ini
-// [TIPS] : Gunakan MCU::<nama methode> untuk mendefinisikan class-nya
+
+MCU::MCU(float cpu_speed, int memory, string os, string name, int volt)
+    : cpu_speed(cpu_speed), memory(memory), os(os), name(name), volt(volt) {
+    cout << "[KONSTRUKTOR MCU] : Objek " << name << " berhasil dibuat." << endl;
+}
+
+MCU::~MCU() {
+    cout << "[DESTRUKTOR MCU] : Objek base " << name << " dibersihkan." << endl;
+}
+
 void MCU::showSpek() {
-    cout << "[BASE MCU] : " << name << " | CPU: " << cpu_speed 
-         << "GHz | RAM: " << memory << "MB | OS: " << os 
+    cout << "[BASE MCU] : " << name << " | CPU: " << cpu_speed
+         << "GHz | RAM: " << memory << "MB | OS: " << os
          << " | Volt: " << volt << "V" << endl;
-};
+}
+
+void MCU::nambah_volt(int penambahan_voltase) {
+    volt += penambahan_voltase;
+    cout << "Voltase " << name << " berhasil dinaikkan ke: " << volt << "V" << endl;
+}
+
+void MCU::ganti_os(string new_os) {
+    os = new_os;
+}
