6e0e58f6a8f7778ba57eff647b4e0d04669771ad
rosianna-maria <rosi.slhi9@gmail.com>
2026-03-25 16:22:03 +0700

    main.cpp

--- soal/soal_1_oop/main.cpp
+++ soal/soal_1_oop/main.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 int main(){
     Core_MCU *device1 = new Core_MCU(1.4,512,"ubuntu 22.04","Raspberry Pi",5);
+    device1->showSpek();
     Core_MCU *device2 = new Core_MCU(4.6,16512,"windows_11","intel_nuc",5);
+    device2->showSpek();
     cout << "Ambil OS [nama OS] : "<< device1->get_os() << endl;
     device1->nambah_volt(2);
     device2->ganti_os("ubuntu 24.04");
