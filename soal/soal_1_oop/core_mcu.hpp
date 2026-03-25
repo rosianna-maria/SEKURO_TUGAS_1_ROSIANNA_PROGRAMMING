a7054975deb3b12696f9a788ae95398ae4be7603
rosianna-maria <rosi.slhi9@gmail.com>
2026-03-25 16:21:39 +0700

    core_mcu.hpp

--- soal/soal_1_oop/core_mcu.hpp
+++ soal/soal_1_oop/core_mcu.hpp
@@ -1,15 +1,17 @@
 #pragma once
-#include "bits/stdc++.h"
-/*[TODO]: Implementasikanlah class berikut didalam file core_mcu.cpp agar main.cpp dapat dijalankan */
+#include <bits/stdc++.h>
 class Core_MCU{
-private:
-    //[TODO]: Buatlah atribute atribute dalam class ini
-public:
-    Core_MCU(float cpu_speed,int memory,std::string os,std::string nama,int volt);
-    void showSpek();
-    ~Core_MCU();
-    void nambah_volt(int penambahan_voltase);
-    void ganti_os(std::string new_os);
-    std::string get_os();
+    private:
+        float cpu_speed;
+        int memory;
+        std::string os;
+        std::string nama;
+        int volt;
+    public:
+        Core_MCU(float cpu_speed, int memory, std::string os, std::string nama, int volt);
+        void showSpek();
+        ~Core_MCU();
+        void nambah_volt(int penambahan_voltase);
+        void ganti_os(std::string new_os);
+        std::string get_os();
 };
-
